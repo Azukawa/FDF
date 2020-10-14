9fa91f818b902c0bc16b90d4839f8c58e1af7083
Azukawa <aleksi.sukava@gmail.com>
2020-10-14 17:30:15 +0300

    line drawing algorythm started

--- fdf.c
+++ fdf.c
@@ -6,30 +6,59 @@
 /*   By: esukava <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/12 13:04:00 by esukava           #+#    #+#             */
-/*   Updated: 2020/10/13 17:03:07 by esukava          ###   ########.fr       */
+/*   Updated: 2020/10/14 16:21:35 by esukava          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
 #include "../_libft/libft.h"
+#include <math.h>
 
 typedef struct			s_program
 {
 	void		*mlx_ptr;
 	void		*win_ptr;
-	int			x;
-	int			y;
-	int			color;
-	int			mx;
-	int			my;
+	int			p0x;
+	int			p0y;
+	int			p1x;
+	int			p1y;
 
+	int			color;
 }						t_program;
+
+int			diagonal_distance(t_program *p);
+{
+	float res;
+
+	res = sprt(pow((p->p0x - p->p1x), 2) + pow((p->p0y - p->p1y), 2));
+
+	return();
+}
+
+
+
+/**put this function in libftlibrary**/
+int			ft_abs(int n)
+{
+	if(n < 0)
+		n = n * -1;
+	return(n);
+}
+
+int			draw_line(t_program *p)
+{
+	int		points = [];
+	int		n = diagonal_distance(p)
+
+
+}
 
 int			deal_mouse(int lol, int x, int y,  t_program *p)
 {
+
+	mlx_pixel_put(p->mlx_ptr, p->win_ptr, x, y, p->color++);
 	p->mx = x;
 	p->my = y;
-	mlx_pixel_put(p->mlx_ptr, p->win_ptr, p->mx, p->my, p->color++);
 	return(0);
 
 }
