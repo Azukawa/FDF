f4339580bb96fa29873ee59c801617556b509d20
Azukawa <aleksi.sukava@gmail.com>
2020-10-19 21:09:18 +0300

    the program now draws wireframe pictures, based on hight maps given. I finnished the function that creates int array from the map file, and also did a drawer function to draw the wireframe itself. The read file function does not skip multiple spaces in a row. Fix this! Good job Aleksi

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: esukava <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/12 13:04:00 by esukava           #+#    #+#             */
-/*   Updated: 2020/10/17 18:12:00 by esukava          ###   ########.fr       */
+/*   Updated: 2020/10/19 21:03:55 by esukava          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,9 @@ typedef struct			s_program
 	t_point_2d	start;
 	t_point_2d	end;
 	int			color;
+	int			**grid;
+	int			gridx;
+	int			gridy;
 }						t_program;
 
 int			diagonal_distance(int p0x, int p0y, int p1x, int p1y)
@@ -118,12 +121,12 @@ void			ft_putintarr(int *arr, int x)
 //
 //}
 
-int			line_to_grid(char *line)
+int			*line_to_grid(char *line, int *grid, int *line_len)
 {
 	int i;
 	int j;
 	int a;
-	char buf[4];
+	char buf[50];
 	int	ret[50];
 	i = 0;
 	j = 0;
@@ -135,24 +138,27 @@ int			line_to_grid(char *line)
 		else
 		{
 			buf[j] = '\0';
-			ret[a] = atoi(buf);
+			grid[a] = ft_atoi(buf);
 			a++;
 			i++;
 			j = 0;
-			ft_bzero(buf, 4);
+			ft_bzero(buf, 50);
 		}
 	}
-	ft_putintarr(ret, a);	
-	return(0);
+	buf[j] = '\0';
+    grid[a] = ft_atoi(buf);
+    a++;
+	*line_len = a;
+//	ft_putintarr(grid, *line_len);
+	return(NULL);
 }
 
-int			read_file(char *str)
+int			read_file(char *str, t_program *p)
 {
 	int		fd;
 	int		ret;
 	char	*output;
 	int		i;
-	int		**grid;
 
 	i = 0;
 	output = NULL;
@@ -160,32 +166,127 @@ int			read_file(char *str)
 	fd = open(str, O_RDONLY);
 	while((ret = get_next_line(fd, &output)) > 0)
 	{
-		line_to_grid(output);
+		line_to_grid(output, p->grid[i], &p->gridx);
 		free(output);
-		ft_putchar('Y');
-		ft_putchar('\n');
+//		ft_putintarr(p->grid[i], p->line_len);
+//		ft_putchar('Y');
+//		ft_putchar('\n');
 		i++;
 	}
+	p->gridy = i;
 	return(0);
 }
 
+int		**init_grid()
+{
+	int **grid;
+	int i;
+	i = 0;
+
+	grid = (int**)malloc(sizeof(int*) * 100000);
+	while(i < 1000)
+		grid[i++] = (int*)malloc(sizeof(int) * 100000);
+	return(grid);
+}
+
+void	draw_left(t_program *p, int i, int j, int x, int y)
+{
+		p->end.x = x;		/*This block draws horizontal*/
+		p->end.y = y - (p->grid[j][i] * 5);
+		if(i > 0)
+			draw_line(p);
+		p->start.x = p->end.x;
+		p->start.y = p->end.y;
+	
+
+}
+
+void	draw_up(t_program *p, int i, int j, int x, int y)
+{		
+		int		temp_startx;
+		int		temp_starty;
+		int		temp_endx;
+		int		temp_endy;
+
+		temp_startx = p->start.x;
+		temp_starty = p->start.y;
+		temp_endx = p->end.x;
+		temp_endy = p->end.y;
+		
+		p->end.x = x + 30;
+		p->end.y = (y - 15) - (p->grid[j - 1][i] * 5);
+		p->start.x = x;
+		p->start.y = y - (p->grid[j][i] * 5);
+			draw_line(p);
+
+		
+		p->start.x = temp_startx;
+		p->start.y = temp_starty;
+		p->end.x = temp_endx;
+		p->end.y = temp_endx;	
+	
+}
+
+void	draw_grid(t_program *p)
+{
+	int		i;
+	int		j;
+	int		x;
+	int		y;
+	int		origin;
+	int		unit;
+	
+	unit = 30;
+	origin = 200;
+	x = origin;
+	y = origin - 150;
+	i = 0;
+	j = 0;
+
+	while(j < p->gridy)
+	{
+		while (i < p->gridx)
+		{
+			mlx_pixel_put(p->mlx_ptr, p->win_ptr, x, y, 0xFFFFFF);
+		//	mlx_pixel_put(p->mlx_ptr, p->win_ptr, x, y + p->grid[j][i], p->color);
+			
+			if(j > 1)	
+				draw_up(p, i, j, x, y);
+
+			draw_left(p, i, j, x, y);
+
+
+
+			x = x + unit;
+			y = y + (unit / 2);
+			i++;
+		}
+		y = (origin - 150) + (j * (unit / 2));
+		x = origin - (j * unit);
+		i = 0;
+		j++;
+	}
+	ft_putstr("xxx");
+}
 
 int		main(int argc, char **argv)
 {
 	t_program	program;
-	int			grid[1000][1000];
+	program.grid = init_grid();
 
 	program.start.x = 250;
 	program.start.y = 300;
-	program.color = 0x000000;	
+	program.color = 0xffa500;	
 
-	read_file(argv[1]);
+	read_file(argv[1], &program);
 
-//	program.mlx_ptr = mlx_init();
-//	program.win_ptr = mlx_new_window(program.mlx_ptr, 500, 500, "Dope!");
+	program.mlx_ptr = mlx_init();
+	program.win_ptr = mlx_new_window(program.mlx_ptr, 1000, 700, "Dope!");
+
+	draw_grid(&program);
 //	mlx_key_hook(program.win_ptr, key_callback, &program);
-//	mlx_mouse_hook(program.win_ptr, mouse_callback, &program);
-//	mlx_loop(program.mlx_ptr);
+	mlx_mouse_hook(program.win_ptr, mouse_callback, &program);
+	mlx_loop(program.mlx_ptr);
 
 }
 
